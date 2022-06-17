cb8828daf08bb3897f426667b5ac2c709013d882
Opa Kholis Majid <code@opakholis.space>
2022-06-17 17:19:56 +0700

    build: update tailwind to v3.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
