4281e8ce2fa31e5aaa26afdea6865ae7952691ae
Opa Kholis Majid <code@opakholis.space>
2022-06-18 08:27:58 +0700

    style: better comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
 
-/** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('tailwindcss').Config}
+ */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
